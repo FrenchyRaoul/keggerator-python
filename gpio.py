b8e1b528c3867a561b443dfd489cbd8c04ebaf63
nlespera <nlespera@us.ibm.com>
2022-09-16 13:42:57 -0400

    major cleanup of fridge code

--- gpio.py
+++ gpio.py
@@ -22,10 +22,19 @@ class Relay(object):
         self.last_update = time.time()
         self.initialize()
 
-
     def initialize(self):
         GPIO.setup(self.relay_pin, GPIO.OUT)
         self.turn_off()
+
+    def _turn_off(self):
+        GPIO.output(self.relay_pin, True)  # 1=off, 0=on
+        self.on = False
+        self.last_update = time.time()
+
+    def _turn_on(self):
+        GPIO.output(self.relay_pin, False)  # 1=off, 0=on
+        self.on = True
+        self.last_update = time.time()
 
     def turn_on(self):
         if self.on:
@@ -33,9 +42,7 @@ class Relay(object):
             return
         down_time = time.time() - self.last_update
         if down_time > RELAY_MIN_OFF_TIME_SECONDS:
-            GPIO.output(self.relay_pin, False)  # 1=off, 0=on
-            self.on = True
-            self.last_update = time.time()
+            self._turn_on()
         else:
             print(f"cannot turn on relay, relay has been off for {down_time} seconds, minimum toggle time is {RELAY_MIN_OFF_TIME_SECONDS} seconds")
 
@@ -44,9 +51,7 @@ class Relay(object):
             print("can't turn off relay, it is already off")
         up_time = time.time() - self.last_update
         if up_time > RELAY_MIN_ON_TIME_SECONDS:
-            GPIO.output(self.relay_pin, True)  # 1=off, 0=on
-            self.on = False
-            self.last_update = time.time()
+            self._turn_off()
         else:
             print(f"cannot turn off relay, relay has been on for {up_time} seconds, minimum toggle time is {RELAY_MIN_ON_TIME_SECONDS} seconds")
 
